b2e26db28b78f24420f94ed69a28c42a01442a9e
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-04-11 11:59:31 +0900

    flatpickr

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,12 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <!-- canで権限管理出来る -->
+                    @can('manager-higher')
+                        <x-jet-nav-link href="{{ route('events.index') }}" :active="request()->routeIs('events.index')">
+                            イベント管理
+                        </x-jet-nav-link>
+                    @endcan
                 </div>
             </div>
 
@@ -139,6 +145,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('events.index') }}" :active="request()->routeIs('events.index')">
+                イベント管理
             </x-jet-responsive-nav-link>
         </div>
 
