bfbc858c4ac09cbd2ab91c059171735cc6618921
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-04-13 11:10:53 +0900

    event past

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -2,26 +2,32 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\DB; //クエリビルダ
+use Illuminate\Support\Facades\DB; 
 use Carbon\Carbon;
 
 class EventService
 {
+  public static function checkEventDuplication($eventDate, $startTime, $endTime)
+  {
+    return DB::table('events')
+        ->whereDate('start_date', $eventDate)
+        ->whereTime('end_date', '>', $startTime)
+        ->whereTime('start_date', '<', $endTime)
+        ->exists();
+   }
 
-    public static function checkEventDuplication($eventDate, $startTime, $endTime)
-    {
-        return DB::table('events') 
-        ->whereDate('start_date', $eventDate) // 日にち 
-        ->whereTime('end_date' ,'>',$startTime) 
-        ->whereTime('start_date', '<',$endTime) 
-        ->exists(); // 存在確認 
-        return $check;
+   public static function countEventDuplication($eventDate, $startTime, $endTime)
+   {
+     return DB::table('events')
+         ->whereDate('start_date', $eventDate)
+         ->whereTime('end_date', '>', $startTime)
+         ->whereTime('start_date', '<', $endTime)
+         ->count();
     }
 
-    public static function joinDateAndTime($date, $time)
-    {
-        $join = $date . " " . $time;
-        return Carbon::createFromFormat('Y-m-d H:i', $join);
-    }
-
+  public static function joinDateAndTime($date, $time)
+  {
+    $join = $date. " " .  $time; 
+    return Carbon::createFromFormat('Y-m-d H:i', $join);
+  }
 }
