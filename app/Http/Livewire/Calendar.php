d0abdc2143e541b0a1bfbb05bf611c8013164958
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-04-27 15:44:59 +0900

    イベントの計算

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -5,56 +5,65 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use Carbon\CarbonImmutable;
 use App\Services\EventService;
+
 class Calendar extends Component
 {
-
     public $currentDate;
     public $currentWeek;
     public $day;
-    public $sevenDayLater;
+    public $checkDay;
+    public $dayOfWeek;
+    public $sevenDaysLater;
     public $events;
-
 
     public function mount()
     {
-        //画面表示時の初期値
         $this->currentDate = CarbonImmutable::today();
-        $this->sevenDayLater = $this->currentDate->addDays(7);
+        $this->sevenDaysLater = $this->currentDate->addDays(7);
         $this->currentWeek = [];
-        
+
         $this->events = EventService::getWeekEvents(
-            $this->currentDate,
-            $this->sevenDayLater->format('Y-m-d'),
+            $this->currentDate->format('Y-m-d'),
+            $this->sevenDaysLater->format('Y-m-d'),
         );
 
-        //7日分の情報を取る
-        for($i = 0; $i < 7; $i++){
+        for($i = 0; $i < 7 ; $i++){
             $this->day = CarbonImmutable::today()->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::today()->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::today()->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek  
+            ]);
         }
 
-        //dd($this->currentWeek);
+        // dd($this->currentWeek);
     }
 
     public function getDate($date)
     {
         $this->currentDate = $date;
         $this->currentWeek = [];
-        $this->sevenDayLater = CarbonImmutable::parse($this->currentDate)->addDays(7);
+        $this->sevenDaysLater = CarbonImmutable::parse($this->currentDate)->addDays(7);
 
         $this->events = EventService::getWeekEvents(
             $this->currentDate,
-            $this->sevenDayLater->format('Y-m-d'),
+            $this->sevenDaysLater->format('Y-m-d'),
         );
 
-        for($i = 0; $i < 7; $i++)
-        {
+        for($i = 0; $i < 7 ; $i++){
             $this->day = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = CarbonImmutable::parse($this->currentDate)->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = CarbonImmutable::parse($this->currentDate)->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek 
+            ]);
         }
+
     }
-
-
     public function render()
     {
         return view('livewire.calendar');
